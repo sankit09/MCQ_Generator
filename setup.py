f5747701ef35792e2ff4e2bcae9b589870a10f96
unknown <you@example.com>
2024-12-17 16:24:50 +0530

    New Changes

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+
+
+setup(
+    name= 'mcqgenerator',
+    version='0.0.1',
+    author="Ankit Srivastava",
+    author_email="sriankit957@gmail.com",
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"]
+    ,packages=find_packages()
+)
